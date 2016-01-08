17d9df2b66fcfeaf72d908a60a8a77b869a3470a
Umair Khan <omerjerk@gmail.com>
2016-01-08 23:35:29 +0530

    Initial app structure
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

---
+++ src/in/omerjerk/shaderflinger/app/MainActivity.java
@@ -0,0 +1,17 @@
+package in.omerjerk.shaderflinger.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.android.calculator2.R;
+
+/**
+ * Created by omerjerk on 8/1/16.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
